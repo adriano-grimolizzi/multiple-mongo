c046a91946e7c2d95612abab077adf1cf7e12bd6
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2021-03-19 18:47:40 +0100

    change step-0 to connect to 2 mongo instances

--- step-0/server.js
+++ step-0/server.js
@@ -9,11 +9,11 @@ const options = {
 // It's possible to use multiple connections using 'mongoose.createConnection()'.
 // It returns a new connection.
 const conn_1 = mongoose.createConnection(
-  "mongodb://localhost/database1",
+  "mongodb://localhost:27017/database1",
   options
 );
 const conn_2 = mongoose.createConnection(
-  "mongodb://localhost/database2",
+  "mongodb://localhost:27018/database1",
   options
 );
 
@@ -21,12 +21,15 @@ const gameSchema = new Schema({ name: String });
 const consoleSchema = new Schema({ name: String });
 
 // Models are always scoped to a single connection
-const GameModel = conn_1.model("Game", gameSchema);
-const ConsoleModel = conn_2.model("Console", consoleSchema);
+const GameModel1 = conn_1.model("Game", gameSchema);
+const GameModel2 = conn_2.model("Game", gameSchema);
+
+// const ConsoleModel = conn_2.model("Console", consoleSchema);
 
 const printElements = (err, elements) => {
   console.log(JSON.stringify(elements, null, 2));
 };
 
-GameModel.find({}, printElements);
-ConsoleModel.find({}, printElements);
+GameModel1.find({}, printElements);
+GameModel2.find({}, printElements);
+// ConsoleModel.find({}, printElements);
